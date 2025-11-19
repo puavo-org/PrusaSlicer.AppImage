ce1bd9ae519cd003695b8b40261a8905be8ebc60
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-19 12:53:31 -0400

    likely needed

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/192x192/apps/PrusaSlicer.png
 export DESKTOP=/usr/share/applications/PrusaSlicer.desktop
+export LOCALE_FIX=1
 
 # Deploy dependencies
 quick-sharun \
