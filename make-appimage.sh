ff42613da6298157c572de28f83099ae2c8d95ed
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-19 12:51:18 -0400

    oops

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/applications/icons/hicolor/192x192/apps/PrusaSlicer.png
+export ICON=/usr/share/icons/hicolor/192x192/apps/PrusaSlicer.png
 export DESKTOP=/usr/share/applications/PrusaSlicer.desktop
 
 # Deploy dependencies
