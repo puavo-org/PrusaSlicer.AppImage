53ed335e4d84c1b10c08226b5851ee72dad132b9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-19 12:46:09 -0400

    package the thing

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q prusa-slicer | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/applications/icons/hicolor/192x192/apps/PrusaSlicer.png
+export DESKTOP=/usr/share/applications/PrusaSlicer.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/prusa-slicer* \
+	/usr/share/PrusaSlicer
 
 # Additional changes can be done in between here
 
