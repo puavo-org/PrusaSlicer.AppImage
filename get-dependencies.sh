ee64d6c1aa2a3f4ecfdab8f709902dea905846dc
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-19 12:42:41 -0400

    get the thing

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm prusa-slicer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
